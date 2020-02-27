9e2ed2c30ee4ff78195e531059a6ef5a17c58bfb
Maria Sevostyanova <letanina.maria@gmail.com>
2020-02-27 09:45:06 +0400

    изменение имени

--- src/test/java/CardOrder.java
+++ src/test/java/CardOrder.java
@@ -3,23 +3,20 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
 
-class CallbackTest {
+
+
+class CardOrder {
         @Test
         void shouldTest() {
-            open("http://localhost:9999");
-            SelenideElement form = form.$("[data-test-id=callback-form]");
+            open("http://localhost:9999/");
+            SelenideElement form = $("[data-test-id=root]");
             form.$("[data-test-id=name] input").setValue("Василий");
             form.$("[data-test-id=phone] input").setValue("+79270000000");
             form.$("[data-test-id=agreement]").click();
             form.$("[data-test-id=submit]").click();
             $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
         }
-
-    private Object exactText(String s) {
-    }
-
-    private void open(String s) {
-    }
-}
 }
