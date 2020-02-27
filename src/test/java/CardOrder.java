7c1b6de9ec7f8db3b1e7bee21a2aa389e62fbe98
Maria Sevostyanova <letanina.maria@gmail.com>
2020-02-27 18:39:50 +0400

    изменения

--- src/test/java/CardOrder.java
+++ src/test/java/CardOrder.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -11,12 +12,12 @@ import static com.codeborne.selenide.Condition.exactText;
 class CardOrder {
         @Test
         void shouldTest() {
-            open("http://localhost:9999/");
-            SelenideElement form = $("[data-test-id=root]");
-            form.$("[data-test-id=name] input").setValue("Василий");
-            form.$("[data-test-id=phone] input").setValue("+79270000000");
-            form.$("[data-test-id=agreement]").click();
-            form.$("[data-test-id=submit]").click();
+            open("http://localhost:9999");
+            SelenideElement form = $("[div id=root]");
+            form.$("[input__control=name] input").setValue("Василий");
+            form.$("[input__control=phone] input").setValue("+79270000000");
+            form.$("[checkbox__control=agreement] input").click();
+            form.$("span.button__text").click();
             $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
         }
 }
