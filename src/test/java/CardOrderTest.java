31718969211958ef431ce50dce2d1b9e3800ade0
Maria Sevostyanova <letanina.maria@gmail.com>
2020-03-02 15:20:10 +0400

    поправки

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -6,7 +6,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Condition.text;
 
-
 class CardOrderTest {
         @Test
         void shouldTest() {
@@ -14,8 +13,8 @@ class CardOrderTest {
             SelenideElement form = $("[id=root]");
             form.$("[name]").setValue("Иванов Василий");
             form.$("[name=phone]").setValue("+79876987564");
-            form.$("span.checkbox__box").click();
-            form.$("button[type=button] ").click();
+            form.$("checkbox__box").click();
+            form.$("button[type=button]").click();
             $("p.paragraph.paragraph_theme_alfa-on-white").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
         }
 }
